6ccd963ba4c30a78ec6f33ed5c3e90b1cc60e0d7
yamada953 <180383902+yamada953@users.noreply.github.com>
2025-11-07 12:09:17 +0900

    バージョンを0.1.2にする

--- lib/cpaas_test_ruby/config.rb
+++ lib/cpaas_test_ruby/config.rb
@@ -1,6 +1,6 @@
 module Karaden
   class Config
-    VERSION = '0.1.1'.freeze
+    VERSION = '0.1.2'.freeze
     DEFAULT_API_BASE = 'https://prg.karaden.jp/api'.freeze
     DEFALUT_API_VERSION = '2024-03-01'.freeze
 
